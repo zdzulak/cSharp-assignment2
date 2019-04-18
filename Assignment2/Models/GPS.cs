f4f080ef0515adc373ce9309c64ab3d67d82f6ae
zdzulak <43354865+zdzulak@users.noreply.github.com>
2019-04-18 10:26:46 -0400

    assignment2

---
+++ Assignment2/Models/GPS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Models
+{
+    class GPS
+    {
+        GeoCoordinateWatcher watcher = new GeoCoordinateWatcher;
+    }
+
+}
---
+++ Assignment2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Location;
+
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
+
+            // Do not suppress prompt, and wait 1000 milliseconds to start.
+            watcher.TryStart(false, TimeSpan.FromMilliseconds(1000));
+
+            GeoCoordinate coord = watcher.Position.Location;
+
+            if (coord.IsUnknown != true)
+            {
+                Console.WriteLine("Lat: {0}, Long: {1}",
+                    coord.Latitude,
+                    coord.Longitude);
+            }
+            else
+            {
+                Console.WriteLine("Unknown latitude and longitude.");
+            }
+        }
+    }
+}
